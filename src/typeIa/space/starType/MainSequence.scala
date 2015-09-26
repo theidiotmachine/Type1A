32255a4a83e8e2ab76605a8ca138e4f0a792d23d
tim <dinsdt@googlemail.com>
2015-09-26 08:16:02 +0100

    Better luminosity and radius calculations

--- src/typeIa/space/starType/MainSequence.scala
+++ src/typeIa/space/starType/MainSequence.scala
@@ -8,12 +8,30 @@ trait MainSequence extends StarType{
     //stefan bolzman (1) - main sequence stars
     //lumiosity = ((mass/SolarMass)^3.9)*solarLuminosity
     //because we are dealing in solar masses and solar luminosities already, this is just this
-    val lum = math.pow(mass, 3.9)
+    //however, 'relation flattens out at higher masses'; www2.astro.psu.edu/users/rbc/a534/lec18.pdf
+    val pow = if(mass > 10)
+      3.0
+    else if(mass < 1)
+      3.0
+    else
+      3.88
+
+    val lum = math.pow(mass, pow)
     if(lum > luminosityMax)
       luminosityMax
     else if(lum < luminosityMin)
       luminosityMin
     else
       lum
+  }
+
+  override def radius(mass: Double): Double = {
+    //from www2.astro.psu.edu/users/rbc/a534/lec18.pdf
+    if(mass == 1.0)
+      1.0
+    else if(mass > 1.0)
+      math.pow(mass, 0.57)
+    else
+      math.pow(mass, 0.8)
   }
 }
